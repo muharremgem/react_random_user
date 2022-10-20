de24b0c0af336730fdb6657158ba9fe1d3a7659e
muharrem gem <muharremgem28@gmail.com>
2022-10-20 16:44:14 +0300

    Background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-indigo-700 to-pink-200  ">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="bos">
         {data ? (
-          <div className="flex flex-col  justify-between items-center w-[30rem] h-[30rem] mx-auto text-center bg-blue-300 p-4">
+          <div className="flex flex-col  justify-between items-center w-[30rem] h-[30rem] mx-auto text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
             <img
               className="rounded-full w-[9rem]"
               src={data.picture.medium}
